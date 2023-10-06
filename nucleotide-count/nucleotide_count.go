71431f7dba21714135efeeb258a8b90bdafa3077
Egit90 <elie90@gmail.com>
2023-10-06 15:30:22 +0200

    new soultion for dna

--- nucleotide-count/nucleotide_count.go
+++ nucleotide-count/nucleotide_count.go
@@ -4,23 +4,43 @@ import (
 	"errors"
 )
 
-type Histogram map[rune]int
+type Nucleotide rune
+
+const (
+	A Nucleotide = 'A'
+	C Nucleotide = 'C'
+	G Nucleotide = 'G'
+	T Nucleotide = 'T'
+)
+
+type Histogram map[Nucleotide]int
 
 type DNA string
 
+func isValidNucleotid(E rune) (bool, Nucleotide) {
+	switch E {
+	case 'A', 'C', 'G', 'T':
+		return true, Nucleotide(E)
+	default:
+		return false, ' '
+	}
+}
+
 func (d DNA) Counts() (Histogram, error) {
 	h := Histogram{
-		'A': 0,
-		'C': 0,
-		'G': 0,
-		'T': 0,
+		A: 0,
+		C: 0,
+		G: 0,
+		T: 0,
 	}
 
 	for _, v := range d {
-		if _, ok := h[v]; !ok {
-			return nil, errors.New("invaild input")
+		ok, nuc := isValidNucleotid(v)
+		if !ok {
+			return nil, errors.New("invalid input")
 		}
-		h[v]++
+
+		h[nuc]++
 	}
 
 	return h, nil
